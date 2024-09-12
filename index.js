9f02d366d398f78548cbaab0156d926d1da2aa19
Rajat47485thakur <Rajat.thakur@rebuild-technologies.com>
2024-09-12 17:02:06 +0530

    db

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json'); // Ensure 'db.json' exists in the same folder
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
 
+server.use(cors()); // Enable CORS if needed
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
